55c0cb07f8968ec6d8b5d2f5c16ee19280ad23b5
Frank Kung <kfanjian@gmail.com>
2017-03-09 22:11:09 +0800

    Write image to graphics file

--- image.go
+++ image.go
@@ -1,14 +1,22 @@
 package gocv
 
 import (
+	"bytes"
+	"errors"
+	"fmt"
 	"image"
+	"image/gif"
 	_ "image/gif"
+	"image/jpeg"
 	_ "image/jpeg"
+	"image/png"
 	_ "image/png"
 	"os"
 
+	"golang.org/x/image/bmp"
 	_ "golang.org/x/image/bmp"
 	_ "golang.org/x/image/riff"
+	"golang.org/x/image/tiff"
 	_ "golang.org/x/image/tiff"
 	_ "golang.org/x/image/vp8"
 	_ "golang.org/x/image/vp8l"
@@ -27,6 +35,34 @@ func Imread(name string) (img *image.Image, format string, err error) {
 	defer f.Close()
 
 	*img, format, err = image.Decode(f)
+
+	return
+}
+
+// Write image to graphics file
+func Imwrite(img *image.Image, name, format string) (err error) {
+	buf := new(bytes.Buffer)
+	switch format {
+	case "jpg", "jpeg":
+		err = jpeg.Encode(buf, *img, nil)
+	case "png":
+		err = png.Encode(buf, *img)
+	case "gif":
+		err = gif.Encode(buf, *img, nil)
+	case "bmp":
+		err = bmp.Encode(buf, *img)
+	case "tiff":
+		err = tiff.Encode(buf, *img, nil)
+	default:
+		err = errors.New("not support")
+	}
+	if err != nil {
+		return
+	}
+
+	w, _ := os.Create(fmt.Sprintf("%s.%s", name, format))
+	fmt.Println(w.Write(buf.Bytes()))
+	fmt.Println(len(buf.Bytes()))
 
 	return
 }
